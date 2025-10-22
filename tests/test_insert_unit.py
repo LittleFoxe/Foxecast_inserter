3691246003f288d40a85d3c7bccfa9116a10a82f
LittleFoxe <vlad.litvintsev@mail.ru>
2025-10-22 17:23:07 +0300

    Removed unnecessary imports from unit test

--- tests/test_insert_unit.py
+++ tests/test_insert_unit.py
@@ -1,9 +1,6 @@
-import shutil
-from pathlib import Path
 from fastapi.testclient import TestClient
 
 from src.main import app
-from src.infrastructure.service_provider import get_downloader
 from src.services.db_service import DatabaseService
 
 
