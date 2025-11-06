bef36ed5fce3e61ac51895152dd80970fc2ecfe1
LittleFoxe <vlad.litvintsev@mail.ru>
2025-11-06 15:13:38 +0300

    Removed redundant import of database service in unit test

--- tests/test_unit.py
+++ tests/test_unit.py
@@ -1,11 +1,9 @@
 from fastapi.testclient import TestClient
 
 from src.main import app
-from src.services.db_service import DatabaseService
 
 
 client = TestClient(app)
-db = DatabaseService()
 
 
 def test_health_ok():
